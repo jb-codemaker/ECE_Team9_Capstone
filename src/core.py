d71e6a623442b470a612b6485641c1410e21b6a7
DrAtomic <leo.ward.garcia@gmail.com>
2021-02-03 12:16:28 -0800

    Revert "Add TODO(#7)"
    This reverts commit 884bfcf5d6cca39715b4e7e3dd2f0346e0e31ffa.

--- src/core.py
+++ src/core.py
@@ -1,27 +1,5 @@
-import cv2
-
-def gray_video():
-    """captures a webcam stream and turns it gray. Hit ESC to quit
-
-
-    """
-    ## TODO(#7): instead of using a webcam play video from file
-    cap = cv2.VideoCapture(0) # the zero means you can capture multiple cameras
-    
-    while(True):
-        
-        ret, frame = cap.read()
-        
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
-        cv2.imshow('frame',gray)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-
-    cap.release()
-    cv2.destroyAllWindows()
+def main():
+    print("hello world")
 
 if __name__ == '__main__':
-    gray_video()
-
+    main()
