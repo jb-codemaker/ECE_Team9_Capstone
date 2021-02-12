faea83da3ee6bc6001e61ffc5640fa3133db50fc
Naiqi Yao <naiqi@pdx.edu>
2021-02-12 15:58:53 -0800

    split video code

---
+++ src/split_video.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+# TODO: allow spaces in path
+def split(file_name, perspective):
+    """Takes a movie file and saves to two mp4 files. One is video only, the other one is audio only.
+
+    ARGS:
+        file_name: video file
+        perspective: either students or teacher
+
+        
+    """
+    if os.name == 'posix':
+        delimiter = '/'
+    else: 
+        delimiter = '\\'
+
+    current_directory = os.getcwd()
+    data_directory = os.path.abspath(os.path.join(current_directory, os.pardir + delimiter + 'data'))
+
+    parent_file =  data_directory + delimiter + file_name
+
+    if os.path.exists(data_directory + delimiter + perspective + '-output-audio.mp4'):
+        os.remove(data_directory + delimiter + perspective + '-output-audio.mp4')
+
+    if os.path.exists(data_directory + delimiter + perspective + '-output-video.mp4'):
+        os.remove(data_directory + delimiter + perspective + '-output-video.mp4')
+
+    os.system('ffmpeg -i '+ parent_file +' -vn -acodec copy '+ data_directory + delimiter + perspective + '-output-audio.mp4 -hide_banner -loglevel error')
+    os.system('ffmpeg -i '+ parent_file +' -c copy -an '+ data_directory + delimiter + perspective + '-output-video.mp4 -hide_banner -loglevel error')
+
+if __name__ == '__main__':
+    file_name = sys.argv[1]
+    perspective = sys.argv[2]
+    split(file_name, perspective)
