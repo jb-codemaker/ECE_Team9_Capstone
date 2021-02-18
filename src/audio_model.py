156d6eeb8f052a95788e2e5733d93b33ddff1a1d
Joshua Blazek <joshua.blazek@gmail.com>
2021-02-18 10:48:43 -0800

    ignore csv. audio updates.

--- src/audio_model.py
+++ src/audio_model.py
@@ -14,10 +14,138 @@ import sys
 
 from os import path
 
-import speech_recognition as sr
+import speech_recognition as sr # Hidden Markov Model (HMM), deep neural network model
 
-import sphinxbase
+#import sphinxbase
 import pocketsphinx
+
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+
+import csv
+import string   #For
+
+from datetime import datetime
+
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), '0-output-audio-original.wav')
+
+myaudio = AudioSegment.from_file(AUDIO_FILE , "wav") 
+chunk_length_ms = 60000 # pydub calculates in millisec
+chunks = make_chunks(myaudio, chunk_length_ms) #Make chunks of one min
+
+for i, chunk in enumerate(chunks):
+    chunk_name = "chunk{0}.wav".format(i)
+    print ("exporting", chunk_name)
+    chunk.export(chunk_name, format="wav")
+
+with open('audio_data_csv.csv', mode='w', newline='') as audio_data_csv:
+    audio_data_csv_writer = csv.writer(audio_data_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    audio_data_csv_writer.writerow(['Translator', 'Minute', 'Speaker', 'WPM', 'Text'])
+
+
+
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("Start of Google Time =", current_time)
+
+
+
+#######  Google test #############
+i = 0
+print ("-------------------------------------------------")
+print ("Testing google's offline recognizer with chunks")
+print ("-------------------------------------------------")
+for chunk in chunks:
+    #chunk_silent = AudioSegment.silent(duration = 10)
+    #audio_chunk = chunk_silent + chunk + chunk_silent
+    chunk.export("./chunk{0}.wav".format(i), bitrate ='192k', format ="wav")
+    filename = 'chunk'+str(i)+'.wav'
+    print("Processing minute "+str(i))
+    file = filename
+    r = sr.Recognizer()
+    with sr.AudioFile(file) as source:
+        #r.adjust_for_ambient_noise(source)
+        audio_listened = r.record(source)
+    try:
+        rec = r.recognize_google(audio_listened)
+        print (rec)
+        word_count = str(rec).split()
+        with open('audio_data_csv.csv', mode='a', newline='') as audio_data_csv:
+            audio_data_csv_writer = csv.writer(audio_data_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            audio_data_csv_writer.writerow(['Google', str(i), 'A', len(word_count), rec])
+    except:
+        rec = r.recognize_google(audio_listened,show_all=True)
+        print(rec,type(rec))
+        word_count = str(rec).split()
+        with open('audio_data_csv.csv', mode='a', newline='') as audio_data_csv:
+            audio_data_csv_writer = csv.writer(audio_data_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            audio_data_csv_writer.writerow(['Google', str(i), 'A', len(word_count), rec])
+
+    i += 1
+############ End Google test ###########
+
+
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("End of Google Time =", current_time)
+
+
+
+############ Testing Sphinx ##########
+i = 0
+print ("\n-------------------------------------------------")
+print ("Testing Sphinx recognizer with chunks")
+print ("-------------------------------------------------")
+for chunk in chunks:
+    #chunk_silent = AudioSegment.silent(duration = 10)
+    #audio_chunk = chunk_silent + chunk + chunk_silent
+    chunk.export("./chunk{0}.wav".format(i), bitrate ='192k', format ="wav")
+    filename = 'chunk'+str(i)+'.wav'
+    print("Processing minute "+str(i))
+    file = filename
+    r = sr.Recognizer()
+    with sr.AudioFile(file) as source:
+        #r.adjust_for_ambient_noise(source)
+        audio_listened = r.record(source)
+    try:
+        rec = r.recognize_sphinx(audio_listened)
+        print (rec)
+        word_count = str(rec).split()
+        with open('audio_data_csv.csv', mode='a', newline='') as audio_data_csv:
+            audio_data_csv_writer = csv.writer(audio_data_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            audio_data_csv_writer.writerow(['Sphinx', str(i), 'A', len(word_count), rec])
+    except:
+        rec = r.recognize_sphinx(audio_listened,show_all=True)
+        print(rec,type(rec))
+        word_count = str(rec).split()
+        with open('audio_data_csv.csv', mode='a', newline='') as audio_data_csv:
+            audio_data_csv_writer = csv.writer(audio_data_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            audio_data_csv_writer.writerow(['Sphinx', str(i), 'A', len(word_count), rec])
+    i += 1
+
+######### End sphinx test ##########
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("End of Sphinx Time =", current_time)
+
+
+
+
+'''
+
+def load_chunks(filename):
+    long_audio = AudioSegment.from_wav(filename)
+    audio_chunks = split_on_silence(
+        long_audio, min_silence_len=1800,
+        silence_thresh=-17
+    )
+    return audio_chunks
 
 # Initialize recognizer class (for recognizing the speech)
 r = sr.Recognizer()
@@ -25,7 +153,7 @@ r = sr.Recognizer()
 # Reading Audio file as source
 # listening the audio file and store in audio_text variable
 
-AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), '0-output-audio.wav')
+
 with sr.AudioFile(AUDIO_FILE) as source:
     
     audio_text = r.listen(source)
@@ -41,7 +169,7 @@ with sr.AudioFile(LONG_AUDIO_FILE) as source:
 
 print('\nTesting Sphinx...\n')
 
-r = sr.Recognizer()
+#r = sr.Recognizer()
 with sr.AudioFile(AUDIO_FILE) as source:
     audio = r.record(source)  # read the entire audio file
 
@@ -83,7 +211,7 @@ AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), '0-output-audio.wa
 recognizer = sr.Recognizer()
 
 def load_chunks(filename):
-    long_audio = AudioSegment.from_mp3(filename)
+    long_audio = AudioSegment.from_wav(filename)
     audio_chunks = split_on_silence(
         long_audio, min_silence_len=1800,
         silence_thresh=-17
@@ -101,3 +229,4 @@ for audio_chunk in load_chunks(AUDIO_FILE):
             print("Error occured")
             print(ex)
 #####------ End split google test---------######
+'''
