f3e0beb28ad923fe59bb27f1727e6456ef020e54
JinghanZhang-Alisa <53472414+JinghanZhang-Alisa@users.noreply.github.com>
2021-02-24 13:36:04 -0800

    Update Test.py

--- src/Test.py
+++ src/Test.py
@@ -1,27 +1,65 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+from numpy import arange
 
-Word_Count = pd.read_csv("data/Sample_csv.csv")
-print(Word_Count)
-
-
-Professor = Word_Count[Word_Count['Speaker'] == 'A']
-Students = Word_Count[Word_Count['Speaker'] == 'B']
-Minute_A = Professor['Minute']
-Speaker_A = Professor['WPM']
-Minute_B = Students['Minute']
-Speaker_B = Students['WPM']
+word_count = pd.read_csv("data/Sample_csv.csv")
+#print(word_count)
 
 
-plt.plot(Minute_A,Speaker_A)
-plt.plot(Minute_B, Speaker_B)
-plt.xlabel('Minutes')
-plt.ylabel('Word/Min')
-plt2=plt.twinx()
-plt2.plot = (Word_Count['Attentiveness'], Word_Count['Minute'])
-plt2.set_ylabel('Attentiveness')
+professor = word_count[word_count['Speaker'] == 'A']
+students = word_count[word_count['Speaker'] == 'B']
+#print(professor)
+#print(students)
+
+max_min = word_count['Minute'].max()+1
+minutes = list(range(max_min))
+
+
+new_index = pd.Index(arange(0,20,1),name='Minute')
+professor.set_index('Minute').reindex(new_index)
+professor.set_index('Minute').reindex(new_index).reset_index()
+
+print(professor)
+'''
+for i in range(len(Minutes)):
+    if professor.isin([i]):
+        print("FOUND")
+'''
+
+print(minutes)
+
+
+
+minute_A = professor['Minute']
+speaker_A = professor['WPM']
+minute_B = students['Minute']
+speaker_B = students['WPM'] 
+
+
+
+
+fig, ax1 = plt.subplots()
+
+
 plt.title('Word numbers and Attentiveness')
+ax1.set_xlabel('Minutes')
+ax1.set_ylabel('Word/Min')
+ax1.plot(minute_A, speaker_A)
+ax1.plot(minute_B, speaker_B)
+
+
+
+ax2=ax1.twinx()
+
+ax2.set_ylabel('Attentiveness')
+ax2.plot = (word_count['Attentiveness'])
+ax2.tick_params()
+
 plt.show()
+
+
+
+
 
 
 
