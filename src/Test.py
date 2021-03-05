53f18c1a26eca9ca8b466fab5676b9516aeda1be
JinghanZhang-Alisa <53472414+JinghanZhang-Alisa@users.noreply.github.com>
2021-03-05 10:54:26 -0800

    Update Test.py

--- src/Test.py
+++ src/Test.py
@@ -1,65 +1,56 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-from numpy import arange
 
+
+# Enter CSV to process:
 word_count = pd.read_csv("data/Sample_csv.csv")
-#print(word_count)
 
+#print(word_count)
 
 professor = word_count[word_count['Speaker'] == 'A']
 students = word_count[word_count['Speaker'] == 'B']
-#print(professor)
-#print(students)
-
-max_min = word_count['Minute'].max()+1
-minutes = list(range(max_min))
-
-
-new_index = pd.Index(arange(0,20,1),name='Minute')
-professor.set_index('Minute').reindex(new_index)
-professor.set_index('Minute').reindex(new_index).reset_index()
-
-print(professor)
-'''
-for i in range(len(Minutes)):
-    if professor.isin([i]):
-        print("FOUND")
-'''
-
-print(minutes)
-
-
-
 minute_A = professor['Minute']
 speaker_A = professor['WPM']
 minute_B = students['Minute']
 speaker_B = students['WPM'] 
 
+df1 = pd.DataFrame(speaker_A)
+df1.columns =['AWPM'] 
+df2 = pd.DataFrame(speaker_B)
+df2.columns =['BWPM'] 
 
+# Merge df1 and df2
+result = pd.concat([df1, df2], axis=1)
+result = result.reset_index(drop=False)
 
+# Replace NaN values with zeroes:
+result.fillna(0, inplace=True)
+result = result.astype(int)
+#print(result)
 
+# Label the plot
 fig, ax1 = plt.subplots()
-
-
+plt.grid()
 plt.title('Word numbers and Attentiveness')
-ax1.set_xlabel('Minutes')
+
+
+# Start to plot
+
+ax1.set_xlabel('Minutes',)
 ax1.set_ylabel('Word/Min')
-ax1.plot(minute_A, speaker_A)
-ax1.plot(minute_B, speaker_B)
+ax1.plot(result['index'], result['AWPM'],label = 'Professor')
+ax1.plot(result['index'], result['BWPM'],label = 'Students')
 
+# Put legend on plot
+plt.legend(loc = (0,1.05))
 
-
+#Plot Attentiveness using different y-axis
 ax2=ax1.twinx()
 
-ax2.set_ylabel('Attentiveness')
-ax2.plot = (word_count['Attentiveness'])
-ax2.tick_params()
+ax2.set_ylabel('Attentiveness',color ='green')
+ax2.plot(word_count['Minute'],word_count['Attentiveness'],'-*', color = 'green',label = 'Attentiveness')
+
+# Put legend on plot
+plt.legend(loc = (0,1))
 
 plt.show()
-
-
-
-
-
-
-
