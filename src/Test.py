cbdfb3db3a1d7b278a2d63c583061ee4d968b326
JinghanZhang-Alisa <53472414+JinghanZhang-Alisa@users.noreply.github.com>
2021-04-22 06:29:30 -0700

    Update Test.py

--- src/Test.py
+++ src/Test.py
@@ -5,7 +5,8 @@ from matplotlib.widgets import CheckButtons
 
 # Enter CSV to process:
 word_count = pd.read_csv(r"ECE_Team9_Capstone\data\Sample_csv.csv")
- 
+
+# Redefine the dataset
 professor = word_count[word_count['Speaker'] == 'A']
 students = word_count[word_count['Speaker'] == 'B']
 minute_A = professor['Minute']
@@ -31,36 +32,40 @@ fig, ax = plt.subplots()
 plt.grid()
 plt.title('Word Count, Ranked Complexity, Attentiveness')
 plt.yticks([])
-
-
+ 
+# Word Count 
 ax1 = ax.twinx()
 ax1.tick_params(axis='y', colors= 'blue')
 ax1.yaxis.set_ticks_position('left')
 ax1.yaxis.set_label_position('left')
 ax1.set_visible(False)
 
+# Attentiveness
 ax2 = ax1.twinx()
 ax2.tick_params(axis='y', colors='green')
 ax2.set_visible(False) 
 
+# Ranked Complexity
 ax3 = ax1.twinx()
 ax3.tick_params(axis='y', colors='red')
 ax3.set_visible(False)  
 ax3.yaxis.set_ticks_position('left')
 ax3.yaxis.set_label_position('left')
 
+# plot all the lines
 P1, = ax1.plot(result['index'], result['AWPM'],'--', color ='blue',label = 'Professor', visible = False)
 P2, = ax1.plot(result['index'], result['BWPM'],color ='blue',label = 'Students', visible = False)
 P3, = ax2.plot(word_count['Minute'], word_count['Attentiveness'], color = 'green',label = 'complex', visible = False)
 P4, = ax3.plot(word_count['Minute'], word_count['Ranked Complexity'], color = 'red',label = 'Ranked Complexity', visible = False)
 
-
+# Check Button
 plt.subplots_adjust(left=0.25, bottom=0.1,right=0.95, top=0.95) 
 labels = ['Professor Word Count', 'Students Word Count', 'Students Attentiveness', 'Ranked Complexity' ]
 activated = [False, False, False, False]
 axCheckButton  = plt.axes([0.03,0.4,0.15,0.15])
 chxbox = CheckButtons(axCheckButton, labels, activated)
 
+### Active lines in different cases
 def set_visable(label):
     index = labels.index(label)
     lines = [P1, P2, P3, P4]
@@ -89,10 +94,10 @@ def set_visable(label):
 
     # Attentiveness 
     if activated[2] == False:
-        ax2.set_ylabel('Attentiveness',color ='green' , visible = False)
+        ax2.set_ylabel('Attentiveness (%)',color ='green' , visible = False)
         ax2.set_visible(False) 
     else:
-        ax2.set_ylabel('Attentiveness',color ='green' , visible = True)
+        ax2.set_ylabel('Attentiveness (%)',color ='green' , visible = True)
         ax2.set_visible(True) 
 
     # Professor and Students Word Count (share same y-axis)
