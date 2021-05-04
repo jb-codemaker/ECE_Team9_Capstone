83e5f67895c9ad2b5aafafce8e383c737fc04791
DrAtomic <leo.ward.garcia@gmail.com>
2021-05-04 01:58:13 -0700

    each student is now a class

--- src/face_analyzer.py
+++ src/face_analyzer.py
@@ -3,14 +3,64 @@ import cv2
 from mtcnn import MTCNN
 import numpy as np
 import math
+import os
 
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.compat.v1.Session(config=config)
 
-#print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-#tf.debugging.set_log_device_placement(True)
+# print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+# tf.debugging.set_log_device_placement(True)
 
+class student:
+    
+    def __init__(self, face, name, img_size):
+        self.face = face
+        self.name = name
+        self.img_size = img_size
+        self.box = face['box']
+        self.face_points = face['keypoints']
+        
+    def __repr__(self):
+        return "student('{}', '{}')".format(self.box, self.name)
+
+    
+    def get_pose_direction(self):
+        focal_length = self.img_size[1]
+        center = (self.img_size[1]/2, self.img_size[0]/2)
+        camera_matrix = np.array(
+        [[focal_length, 0, center[0]],
+         [0, focal_length, center[1]],
+         [0, 0, 1]], dtype = "double"
+    )
+        model_points = np.array([
+            (0.0, 0.0, 0.0),             # Nose tip
+            (-225.0, 170.0, -135.0),     # Left eye left corner
+            (225.0, 170.0, -135.0),      # Right eye right corne
+            (-150.0, -150.0, -125.0),    # Left Mouth corner
+            (150.0, -150.0, -125.0)      # Right mouth corner
+        ])
+        dist_coeffs = np.zeros((4,1))
+        image_points = np.array([
+            self.face_points['nose'],        
+            self.face_points['left_eye'],    
+            self.face_points['right_eye'],   
+            self.face_points['mouth_left'],  
+            self.face_points['mouth_right']  
+        ], dtype="double")
+        (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_UPNP)
+        
+        (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+
+        #for p in image_points:
+        #    cv2.circle(im, (int(p[0]), int(p[1])), 3, (0, 0, 255), -1)
+      
+        p1 = (int(image_points[0][0]), int(image_points[0][1]))
+        p2 = (int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+        return (p1, p2)
+    
+    
+    
 def find_faces(img):
     """
     Find the faces in an image
@@ -25,7 +75,7 @@ def find_faces(img):
 
     """
     min_conf = 0.9
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     pixels = np.asarray(img)
 
     detector = MTCNN()
@@ -36,55 +86,6 @@ def find_faces(img):
 
 
 
-def get_pose_direction(faces,img_size, camera_matrix, dist_coeffs):
-    """analyzes keypoints from faces and returns direction
-
-    Args:
-       faces: list dictsionaries of faces stuff
-    
-       img_size: img.shape
-
-    Returns:
-        list of directions
-
-    """
-    model_points = np.array([
-        (0.0, 0.0, 0.0),             # Nose tip
-        (-225.0, 170.0, -135.0),     # Left eye left corner
-        (225.0, 170.0, -135.0),      # Right eye right corne
-        (-150.0, -150.0, -125.0),    # Left Mouth corner
-        (150.0, -150.0, -125.0)      # Right mouth corner
-    ])
-    
-    p1 = []
-    p2 = []
-    rotation_vector_list = []
-    translation_vector_list = []
-    
-    for face in faces:
-        image_points = np.array([
-            face['keypoints']['nose'],        # Nose tip
-            face['keypoints']['left_eye'],    # Left eye left corner
-            face['keypoints']['right_eye'],   # Right eye right corne
-            face['keypoints']['mouth_left'],  # Left Mouth corner
-            face['keypoints']['mouth_right']  # Right mouth corner
-        ], dtype="double")
-        
-        
-        (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_UPNP)
-        (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
-        rotation_vector_list.append(rotation_vector)
-        translation_vector_list.append(translation_vector)
-        
-        for p in image_points:
-            cv2.circle(im, (int(p[0]), int(p[1])), 3, (0,0,255), -1)
-      
-        p1.append((int(image_points[0][0]), int(image_points[0][1])))
-        p2.append((int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1])))
-        
-    return(p1, p2, rotation_vector_list, translation_vector_list)
-
-
 def iter_images(folder_path):
     """iterates through a folder and returns an attentiveness
 
@@ -93,42 +94,52 @@ def iter_images(folder_path):
 
 
     """
-    pass
+    os.chdir(folder_path)
+    for filename in os.listdir(folder_path):
+        print(filename)
+        im = cv2.imread(filename)
+        img_size = im.shape
+        faces = find_faces(im)
+        student_list = []
+        for i in range(len(faces)):
+            student_list.append(student(faces[i], str(i), img_size))
 
+        for i in student_list:
+            student_pose = i.get_pose_direction()
+            cv2.line(im, student_pose[0], student_pose[1], (255, 0, 0), 2)
+            
+        cv2.imshow("output",im)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    os.chdir(os.path.relpath("../../src/"))
+
+# TODO: get_angle function, make a function to get an angle for a particular student
+# TODO: get_mode_angle, make a function get most common angle
+# TODO: check_if_same_student, make a function that returns true if the student is the same, then appends angle to list of angles
+# TODO: attentiveness_for_frame, gets the ratio of current angle for frame / mode angle
 if __name__ == '__main__':
+    if os.name == 'posix':
+        delimiter = '/'
+    else: 
+        delimiter = '\\'
+    current_directory = os.getcwd()
+    data_directory = os.path.abspath(os.path.join(current_directory, os.pardir + delimiter + 'data'))
+    screenshot_directory = os.path.abspath(os.path.join(data_directory + delimiter + 'screenshot'))
+
+    iter_images(screenshot_directory)
     
-    im = cv2.imread("/home/leo/Projects/ECE_Team9_Capstone/data/screenshot/screenshot-006.png")
-    img_size = im.shape
-    font = cv2.FONT_HERSHEY_SIMPLEX 
-    focal_length = img_size[1]
-    center = (img_size[1]/2, img_size[0]/2)
-    camera_matrix = np.array(
-        [[focal_length, 0, center[0]],
-         [0, focal_length, center[1]],
-         [0, 0, 1]], dtype = "double"
-    )
-
-    dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
-
-    faces = find_faces(im)
-
-    ang1 = []
-
-    p1, p2, rotation_vector, translation_vector = get_pose_direction(faces, img_size, camera_matrix, dist_coeffs)
-    
-    j = 0
-    for point1, point2 in zip(p1, p2):
-        cv2.line(im, point1, point2, (255,0,0), 2)
-        try:
-            m = (point2[1] - point1[1]/(point2[0] - point1[0]))
-            ang1.append(int(math.degrees(math.atan(m))))
-        except:
-            ang1.append(90)
-        #cv2.putText(im, str(ang1[j])+'ang1', tuple(point1), font, 0.5, (128, 255, 255), 1)
-        j+=1
+    # j = 0
+    # for point1, point2 in zip(p1, p2):
+    #     cv2.line(im, point1, point2, (255, 0, 0), 2)
+    #     try:
+    #         m = (point2[1] - point1[1]/(point2[0] - point1[0]))
+    #         ang1.append(int(math.degrees(math.atan(m))))
+    #     except:
+    #         ang1.append(90)
+    #     #cv2.putText(im, str(ang1[j])+'ang1', tuple(point1), font, 0.5, (128, 255, 255), 1)
+    #     j+= 1
 
     # Display image
-    cv2.imshow("Output", im)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
+    #cv2.imshow("Output", im)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows() 
